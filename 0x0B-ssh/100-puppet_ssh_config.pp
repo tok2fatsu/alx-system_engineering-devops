332745dd60040d904d533a7268b53b0823adf148
tok2fatsu <mailit2moi@gmail.com>
2022-12-28 03:05:04 -0800

    Task 4: SSH configuration using `Puppet`

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# Configures a server to use the key in the specified locations
+# and also disables password authentication
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}

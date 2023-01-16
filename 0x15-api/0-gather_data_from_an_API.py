5d29bccbd65388dbc7fba2cd601e470a33e9e6ac
tok2fatsu <mailit2moi@gmail.com>
2023-01-16 12:15:56 -0800

    fixing bug

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Using https://jsonplaceholder.typicode.com
+Using https://lasonplaceholder.typicode.com
 """
 import re
 import requests
@@ -11,21 +11,21 @@ API = "https://jsonplaceholder.typicode.com"
 """REST API url"""
 
 
-if __name__ == '__main--':
+if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
             id = int(sys.argv[1])
             user_res = requests.get('{}/users/{}'.format(API, id)).json()
             todos_res = requests.get('{}/todos'.format(API)).json()
             user_name = user_res.get('name')
-            todos = list(filter(lambda x: x.get('userID') == id, todos_res))
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
             todos_done = list(filter(lambda x: x.get('completed'), todos))
             print(
-                    'Employee {} is done with tasks({}/{}:'.format(
-                        user_name,
-                        len(todos_done),
-                        len(todos)
-                        )
-                    )
+                'Employee {} is done with tasks({}/{}):'.format(
+                    user_name,
+                    len(todos_done),
+                    len(todos)
+                )
+            )
             for todo_done in todos_done:
                 print('\t {}'.format(todo_done.get('title')))

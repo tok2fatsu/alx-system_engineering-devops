4bae237f2ffd4b241ee244d4705f13396e9c9683
tok2fatsu <mailit2moi@gmail.com>
2023-01-16 12:21:04 -0800

    fixing bug

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,94 @@
 #!/usr/bin/python3
+"""0-gather_data_from_an_API model
 """
-Using https://lasonplaceholder.typicode.com
-"""
-import re
+import json
 import requests
-import sys
-
+from sys import argv
 
 API = "https://jsonplaceholder.typicode.com"
-"""REST API url"""
 
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API, id)).json()
-            todos_res = requests.get('{}/todos'.format(API)).json()
-            user_name = user_res.get('name')
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            todos_done = list(filter(lambda x: x.get('completed'), todos))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    user_name,
-                    len(todos_done),
-                    len(todos)
-                )
-            )
-            for todo_done in todos_done:
-                print('\t {}'.format(todo_done.get('title')))
+class Todo:
+    """Todo Class
+    """
+
+    def __init__(self, userId, id, title, completed):
+        """initializes a todo class
+        Args:
+            userId (int): the user id
+            id (id): the task id
+            title (str): the task title
+            completed (boolean): boolean completed flag
+        """
+        self.userId = userId
+        self.id = id
+        self.title = title
+        self.completed = completed
+
+
+class User:
+    """user class
+    """
+
+    def __init__(self, id, name):
+        """initializes a user class
+        Args:
+            id (int): the user id
+            name (str): the user name
+        """
+        self.id = id
+        self.name = name
+
+
+def get_employee_todos(id):
+    """gets employee data
+    Args:
+        id (int): the employee id
+    Returns:
+        list | None: list of todos or None
+    """
+    result = requests.get("{}/todos?userId={}".format(API, id))
+    if result.status_code == 200:
+        todos = json.loads(result.text)
+        return [Todo(**todo) for todo in todos]
+
+
+def get_user_info(id):
+    """gets user information
+    Args:
+        id (int): the user id
+    Returns:
+        User | None: user or None
+    """
+    result = requests.get("{}/users/{}".format(API, id))
+    if result.status_code == 200:
+        user_data = json.loads(result.text)
+        return User(
+            id=user_data["id"],
+            name=user_data["name"],
+        )
+
+
+def print_employee_todos(employee, todos):
+    """prints employee completed todos
+    Args:
+        employee (user): user
+        todos (list(todo)): list of todos
+    """
+    done_todos = [todo for todo in todos if todo.completed]
+    total_count = len(todos)
+    done_count = len(done_todos)
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee.name,
+        done_count,
+        total_count,
+    ))
+    for todo in done_todos:
+        print("\t {}".format(todo.title))
+
+
+if __name__ == "__main__":
+    if (len(argv) > 1 and argv[1].isdigit()):
+        todos = get_employee_todos(int(argv[1]))
+        employee = get_user_info(argv[1])
+        print_employee_todos(employee, todos)

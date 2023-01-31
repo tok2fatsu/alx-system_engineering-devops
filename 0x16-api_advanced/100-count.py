18e93c7ebe743d82aab5c327faee3ea47e596cef
tok2fatsu <mailit2moi@gmail.com>
2023-01-31 16:15:26 +0000

    Task 3: A recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""100-count
+"""
+import json
+from operator import itemgetter
+
+import requests
+
+BASE_URL = "https://www.reddit.com/r"
+HEADERS = {"User-Agent": "ALX-Bot"}
+
+
+def count_words(subreddit, word_list, after=None, counter=None):
+    """recursively calls the reddit api to get list hot posts and count the
+    given keyword in their title
+    """
+    if len(word_list) == 0:
+        return
+
+    if counter is None:
+        counter = {}
+        for word in word_list:
+            if word.lower() not in counter:
+                counter[word.lower()] = 0
+
+    query = "" if after is None else "&after={}".format(after)
+    result = requests.get(
+        "{}/{}/hot.json?limit=100{}".format(BASE_URL, subreddit, query),
+        headers=HEADERS,
+        allow_redirects=False,
+    )
+
+    if result.status_code == 200:
+        data = json.loads(result.text)["data"]
+
+        for post in data["children"]:
+            title = post.get('data').get("title").lower().split()
+            for word in word_list:
+                counter[word.lower()] += sum(w == word.lower() for w in title)
+
+        after = data["after"]
+        if after is None:
+            counter = sorted(
+                sorted(counter.items()),
+                key=itemgetter(1),
+                reverse=True,
+            )
+            for word in counter:
+                if word[1] > 0:
+                    print("{}: {}".format(word[0], word[1]))
+        else:
+            count_words(subreddit, word_list, after, counter)
